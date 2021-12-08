539e5532dced4a482e024a3fb20ed8ad8861834e
Kjisu <gabrieljisu9@gmail.com>
2021-12-08 15:29:50 +0900

    initial commit

---
+++ src/main/java/org/zerock/boardWithReply/BoardWithReplyApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.boardWithReply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWithReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWithReplyApplication.class, args);
+	}
+
+}
